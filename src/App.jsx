931a85b9b49e8919abac87b81657ac74c7882999
Jane <maryjanesalem@outlook.ph>
2023-02-15 23:31:15 +0800

    added favicon, changed banners to hq

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
 
   return (
     <>
+
+      {/* Navbar */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">GameHub</a>
@@ -37,144 +39,186 @@ function App() {
         </div>
       </nav>
 
+      {/* Header Banner */}
       <header>
-        <div className="browse container">
-          <h1 className="text-white gameon">Get your game on!</h1>
-          <button type="button" class="btn btn-warning">Browse Here</button>
+        <div className="browse container ">
+          <div className="">
+            <h1 className="text-white gameon">Get your game on!</h1>
+            <button type="button" className="btn btn-warning btn-lg">Browse Games</button>
+          </div>
         </div>
-
       </header>
 
 
 
+      {/* MAIN */}
+      <main className="">
+        <div className="container py-5">
 
-
-      <section className="for-carousel">
-        <div className="container h-50 mt-5">
-          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="banner1.png" class="d-block w-100" alt="..." />
-              </div>
-              <div class="carousel-item">
-                <img src="banner2.png" class="d-block w-100" alt="..." />
-              </div>
-              <div class="carousel-item">
-                <img src="banner1.png" class="d-block w-100" alt="..." />
+          {/* Main > First Carousel */}
+          <section className="for-carousel">
+            <div className="container h-50 mt-5">
+              <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src="images/banner1.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="images/banner2.png" className="d-block w-100" alt="..." />
+                  </div>
+                  <div className="carousel-item">
+                    <img src="images/banner3.png" className="d-block w-100" alt="..." />
+                  </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
               </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+          </section>
+
+
+
+
+
+          {/* Main > New Release Games*/}
+          <h1 className="my-5 text-secondary">New Release</h1>
+          <div className="row">
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 1</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 2" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 2</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 3" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 2</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 4" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 3</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
           </div>
+
+          {/* Main > Popular games */}
+          <h1 className="my-5 text-secondary">Popular</h1>
+          <div className="row">
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 1</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 2" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 2</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 3" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 2</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 mb-4">
+              <div className="card border-primary">
+                <img src="images/games/console.jpg" className="card-img-top" alt="Game 4" />
+                <div className="card-body">
+                  <h5 className="card-title">Game 3</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <a href="#" className="btn btn-secondary">Buy Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Browse Game Mid Main */}
+          <section>
+            <div className="container text-end my-5">
+              <div className="">
+                <h1 className="text-warning">Discover your new favorite game!</h1>
+                <button type="button" className="btn btn-warning btn-lg">Browse Games</button>
+              </div>
+            </div>
+          </section>
+
+          {/* Main > Second Carousel */}
+          {/* <section>
+          <div className="container h-50 pt-5 mt-5">
+            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src="images/banner1.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="images/banner2.png" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                  <img src="images/banner3.png" className="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </section> */}
+
         </div>
 
-      </section>
 
-
-
-      <main class="container py-5">
-
-        <h1 class="my-5 text-secondary">New Release</h1>
-
-
-        <div class="row">
-          <div class="col-md-6 col-lg-3 mb-4">
-            <div class="card border-primary">
-              <img src="console.jpg" class="card-img-top" alt="Game 1" />
-              <div class="card-body">
-                <h5 class="card-title">Game 1</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <a href="#" class="btn btn-secondary">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3 mb-4">
-            <div class="card border-primary">
-              <img src="console.jpg" class="card-img-top" alt="Game 2" />
-              <div class="card-body">
-                <h5 class="card-title">Game 2</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <a href="#" class="btn btn-secondary">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3 mb-4">
-            <div class="card border-primary">
-              <img src="console.jpg" class="card-img-top" alt="Game 3" />
-              <div class="card-body">
-                <h5 class="card-title">Game 2</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <a href="#" class="btn btn-secondary">Buy Now</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-3 mb-4">
-            <div class="card border-primary">
-              <img src="console.jpg" class="card-img-top" alt="Game 4" />
-              <div class="card-body">
-                <h5 class="card-title">Game 3</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <a href="#" class="btn btn-secondary">Buy Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <h1 class="my-5 text-secondary">Popular</h1>
-
-
-<div class="row">
-  <div class="col-md-6 col-lg-3 mb-4">
-    <div class="card border-primary">
-      <img src="console.jpg" class="card-img-top" alt="Game 1" />
-      <div class="card-body">
-        <h5 class="card-title">Game 1</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a href="#" class="btn btn-secondary">Buy Now</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-3 mb-4">
-    <div class="card border-primary">
-      <img src="console.jpg" class="card-img-top" alt="Game 2" />
-      <div class="card-body">
-        <h5 class="card-title">Game 2</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a href="#" class="btn btn-secondary">Buy Now</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-3 mb-4">
-    <div class="card border-primary">
-      <img src="console.jpg" class="card-img-top" alt="Game 3" />
-      <div class="card-body">
-        <h5 class="card-title">Game 2</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a href="#" class="btn btn-secondary">Buy Now</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-3 mb-4">
-    <div class="card border-primary">
-      <img src="console.jpg" class="card-img-top" alt="Game 4" />
-      <div class="card-body">
-        <h5 class="card-title">Game 3</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <a href="#" class="btn btn-secondary">Buy Now</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
+        {/* <section className="container-fluid my-5">
+          <img src="images/banner-wide1.png" alt="" />
+        </section> */}
 
 
 
